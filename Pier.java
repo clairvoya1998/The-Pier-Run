832aa3c3aae26cada5fbaefe8a669ce5c3e8a756
kirstyhmcguire <kirstyh.mcguire@googlemail.com>
2016-10-08 16:25:25 +0100

    Add files via upload

--- Pier.java
+++ Pier.java
@@ -5,6 +5,7 @@ import javax.swing.Timer;
 import java.awt.Graphics2D;
 import java.util.Random;
 import java.lang.Math;
+import java.awt.Font;
 
 public class Pier {
     private final float GRAVITY = 9f;
@@ -59,33 +60,52 @@ public class Pier {
     }
     private int score = 0;
     public void drawOn(Graphics2D g2d) {
-    	score++;
-        y += GRAVITY;
-        y += dy;
+    	if (!dead) {
+    		score++;
+    	
+    		y += GRAVITY;
+    		y += dy;
         
-        if(y <= 140) {
-          dy = 0;
-        }
-        for (int i = 0; i < 5; i++) {
-            obx[i] -= 8;
-            while (obx[i] < 0) {
-                obx[i] += 1000 + randInt(0, 3000);
-                for (int j = 0; j < 5; j++) {
-                    while (Math.abs(obx[i] - obx[j]) < 200 && i != j) obx[i] += 350;
-                }
-            }
-        }
-        for (int i = 0; i < 5; i++) {
-            if (obx[i] < 1000) g2d.drawRect(obx[i], 270, 50, 50);
-        }
-        g2d.drawLine(0, 320, 1000, 320);
-        y = Math.min(y,260);
+    		if(y <= 140) {
+    			dy = 0;
+    		}
+    		for (int i = 0; i < 5; i++) {
+    			obx[i] -= 8;
+    			while (obx[i] < 0) {
+    				obx[i] += 1000 + randInt(0, 3000);
+    				for (int j = 0; j < 5; j++) {
+    					while (Math.abs(obx[i] - obx[j]) < 200 && i != j) obx[i] += 350;
+    				}
+    			}
+    		}
+        	for (int i = 0; i < 5; i++) {
+        		if (obx[i] < 1000) g2d.drawRect(obx[i], 270, 50, 50);
+        	}
+        	g2d.drawLine(0, 320, 1000, 320);
+        	y = Math.min(y,260);
         
-        g2d.drawString("Score: " + score, 10, 10);
-        g2d.drawImage(this.image, this.x, this.y, null);
-        dead = cDetect();
+        	g2d.drawString("Score: " + score, 10, 10);
+        	g2d.drawImage(this.image, this.x, this.y, null);
+        	dead = cDetect();
+        } else
+			gameOver(g2d, score);
     }
     
+    private void gameOver(Graphics2D g, long score) {
+		Font gameOverFont = new Font("Sans-Serif",1,70);
+		g.setFont(gameOverFont);
+		g.drawString("GAME OVER", 270, 200);
+		Font scoreFont = new Font("Sans-Serif",1,40);
+		g.setFont(scoreFont);
+		score-=1;
+		g.drawString("Your score was: "+score, 270, 300);
+		Font optionsFont = new Font("Sans-Serif",1,20);
+		g.setFont(optionsFont);
+		g.drawString("Do you want to play again? Y/N", 330, 400);
+		
+	}
+    
+	
     public void keyPressed(KeyEvent e) {
 
         int key = e.getKeyCode();
